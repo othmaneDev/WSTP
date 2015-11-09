79bce81e7ae0e71f724fdbf57e63304e18d01c85
omahboub <mahboubensias@email.com>
2015-11-09 17:18:24 +0100

    xsa Commit for testing only, pleqse ignore

--- src/stage/wstp/controllers/Category.java
+++ src/stage/wstp/controllers/Category.java
@@ -1,14 +1,12 @@
 package stage.wstp.controllers;
 
 import java.io.IOException;
-
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import stage.wstp.model.daos.CategoryDAO;
 import stage.wstp.model.daos.TagDAO;
 
